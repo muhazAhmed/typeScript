2f14930dff6bcb86d35a73efbeed372212215654
Muhaz <muhazvla313@gmail.com>
2022-12-03 17:19:35 +0530

    Basics of types

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,19 @@
-var character = 'Hello!';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var char = "Muhaz";
+// char = 10
+char = "Hello";
+// Here we can change the type of chareter only to chareter itelf.
+var number = 10;
+number = 30;
+// number = "Hello"
+//same goes for number (Int) also
+//but the best this about typescript is, we can handle few edge cases before execution itself like:
+var solution = function (number) {
+    return number + 10;
+};
+console.log(solution(number));
+//this is usual js code....
+var result = function (n) {
+    return n + "hello";
+};
+console.log(result("world"));
+//here you can see that we are getting error. This is the benifit of typeScript
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,26 @@
-const character = 'Hello!';
+let char = "Muhaz"
 
-console.log(character);
+// char = 10
+char = "Hello"
+// Here we can change the type of chareter only to chareter itelf.
 
-const inputs = document.querySelectorAll('input');
+let number = 10
 
-inputs.forEach(input => {
-  console.log(input);
-});
+number = 30
+// number = "Hello"
+//same goes for number (Int) also
+
+//but the best this about typescript is, we can handle few edge cases before execution itself like:
+
+const solution = (number) => {
+  return number + 10
+}
+console.log(solution(number))
+
+//this is usual js code....
+
+const result = (n : number) => {
+  return n + "hello"
+}
+console.log(result("world"))
+//here you can see that we are getting error. This is the benifit of typeScript
